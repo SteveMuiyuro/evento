7930530da713132183e9cb4a89eb78b29c29a735
SteveMuiyuro <stevemuiyuro@gmail.com>
2024-02-25 14:51:08 +0300

    Add clsx functionality to the header link items

--- Desktop/projects/evento/components/Header.tsx
+++ Desktop/projects/evento/components/Header.tsx
@@ -1,6 +1,11 @@
+"use client"
+
+
 import Link from 'next/link'
 import React from 'react'
 import Logo from './Logo'
+import { usePathname } from 'next/navigation'
+import clsx from 'clsx'
 
 const routes = [
 {
@@ -15,12 +20,17 @@ const routes = [
 ]
 
 export default function Header() {
+
+  const activePathName = usePathname()
   return (
     <header className='flex items-center h-14 justify-between border-b border-white/10 px-3 sm:px-9'>
         <Logo/>
         <nav>
           <ul className='flex gap-x-6 text-sm'>
-            {routes.map(route => (<li className="text-white/50 hover:text-white transition"key={route.path}>
+            {routes.map(route => (<li className={clsx("hover:text-white transition", {
+              "text-white" : activePathName === route.path,
+              "text-white/50":activePathName !== route.path
+            })}key={route.path}>
             <Link href={route.path}>{route.name}</Link>
             </li>))}
           </ul>
