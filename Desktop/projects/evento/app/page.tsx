a66ddf00d536953d3420fb6e9572890f8132d67c
SteveMuiyuro <stevemuiyuro@gmail.com>
2024-02-25 15:36:25 +0300

    Add handlesubmit function inside the searchform component

--- Desktop/projects/evento/app/page.tsx
+++ Desktop/projects/evento/app/page.tsx
@@ -1,3 +1,4 @@
+import SearchForm from '@/components/SearchForm'
 import Image from 'next/image'
 import Link from 'next/link'
 
@@ -6,10 +7,7 @@ export default function Home() {
     <main className='flex flex-col items-center px-3 pt-36' >
     <h1 className='text-3xl lg:text-6xl font-bold tracking-tight'>Find events around you</h1>
     <p className='mb-12 mt-7 text-2xl lg:text-3xl opacity-75'>Browsse more than <span className='font-bold italic  underline text-accent'>10,000</span> events around you</p>
-    <form className='w-full sm:w-[580px]' >
-      <input className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-accent/50 transition focus:ring-2 focus:bg-white/10 " spellCheck={false} placeholder="Serach events in any city..."/>
-
-    </form>
+        <SearchForm/>
 
     <section className='mt-4 flex gap-x-4 text:sm text-white/50'>
       <p>Popular:</p>
