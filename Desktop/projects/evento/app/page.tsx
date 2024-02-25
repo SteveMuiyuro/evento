c54bef723bea98bdfb7169c3fd791e705858e0af
SteveMuiyuro <stevemuiyuro@gmail.com>
2024-02-25 14:16:18 +0300

    Include accent color inside the tailwind configuration file

--- Desktop/projects/evento/app/page.tsx
+++ Desktop/projects/evento/app/page.tsx
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <main className='flex flex-col items-center px-3 pt-36' >
     <h1 className='text-3xl lg:text-6xl font-bold tracking-tight'>Find events around you</h1>
-    <p className='mb-12 mt-7 text-2xl lg:text-3xl opacity-75'>Browsse more than <span className='font-bold italic  underline text-[#a4f839]'>10,000</span> events around you</p>
+    <p className='mb-12 mt-7 text-2xl lg:text-3xl opacity-75'>Browsse more than <span className='font-bold italic  underline text-accent'>10,000</span> events around you</p>
     <form className='w-full sm:w-[580px]' >
-      <input className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-[#a4f839]/50 transition focus:ring-2 focus:bg-white/10 " spellCheck={false} placeholder="Serach events in any city..."/>
+      <input className="w-full h-16 rounded-lg bg-white/[7%] px-6 outline-none ring-accent/50 transition focus:ring-2 focus:bg-white/10 " spellCheck={false} placeholder="Serach events in any city..."/>
 
     </form>
 
