e8adb8717fea289c0391387bb9f00c4b162440d1
SteveMuiyuro <stevemuiyuro@gmail.com>
2024-02-26 13:43:00 +0300

    Make the fetch link dynamic

--- Desktop/projects/evento/app/events/[city]/page.tsx
+++ Desktop/projects/evento/app/events/[city]/page.tsx
@@ -9,11 +9,11 @@ type EventsProps = {
 }
 export default async function Events({params}:EventsProps) {
 
-  const res = await fetch("https://bytegrad.com/course-assets/projects/evento/api/events?city=austin")
+  const city = params.city
+
+  const res = await fetch(`https://bytegrad.com/course-assets/projects/evento/api/events?city=${city}`)
 
   const events:EventEvento[] = await res.json()
-
-  const city = params.city
 
   return (
     <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
