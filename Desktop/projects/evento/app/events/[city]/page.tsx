2a30101e4fe5b1a4ba0a0be4f84b12e4d35443ba
SteveMuiyuro <stevemuiyuro@gmail.com>
2024-02-26 10:19:39 +0300

    Link params.cty to the H1

--- Desktop/projects/evento/app/events/[city]/page.tsx
+++ Desktop/projects/evento/app/events/[city]/page.tsx
@@ -1,10 +1,22 @@
 import H1 from '@/components/H1'
-import React from 'react'
 
-export default function Events() {
+type EventsProps = {
+  params:{
+    city:string
+  }
+}
+export default function Events({params}:EventsProps) {
+
+  const city = params.city
+
+  console.log(city)
+
   return (
-    <main>
-      <H1>Your Events Results</H1>
+    <main className="flex flex-col items-center py-24 px-[20px] min-h-[110vh]">
+      <H1>{
+       city === "all" ? "All Events" : `Events in ${city.charAt(0).toUpperCase() + city.slice(1)}`
+
+        }</H1>
     </main>
   )
 }
--- Desktop/projects/evento/app/layout.tsx
+++ Desktop/projects/evento/app/layout.tsx
@@ -19,7 +19,7 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`S{inter.className} bg-gray-900 text-white`}>
+      <body className={`S{inter.className} bg-gray-900 text-white overflow-y-scroll`}>
        <Container>
           <Header/>
             {children}
